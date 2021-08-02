823997e164c97ba6abb9db9db3a0cfa2da77fe21
bes1337 <cauze4concern@yandex.ru>
2021-08-02 17:43:22 +0700

    2

---
+++ observer/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "observer.hpp"
+
+struct Button
+{
+    enum {
+        pressed = 0,
+        released
+    };
+    int state;
+};
+
+class Event_Handler1 {
+
+};
+
+
+int main() {
+    return 0;
+}
---
+++ observer/observer.hpp
@@ -0,0 +1,33 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+typedef unsigned int size_t;
+
+namespace bes {
+    class IObserver {
+        virtual void notify() = 0;
+    };
+
+    class ISubject {
+        virtual void accept() = 0;
+    };
+
+    //template <typename T, const size_t MAX_OBSERVERS>
+    //Client 
+    class Observer: public IObserver {
+    public:
+        virtual void notify();
+    };
+
+    class Subject: public ISubject {
+    private:
+        size_t max_observers;
+        size_t observers[3];
+    public:
+        virtual void accept(IObserver& obs);
+        void notify_observers();
+    };
+};
+
+
+#endif
